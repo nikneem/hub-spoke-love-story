c640d34adfe6fe977b5c9290684892ae8fffba43
nikneem <ekeilholz+git@outlook.com>
2025-06-02 10:56:14 +0200

    Updated project

--- src/Demo.WebApi/Program.cs
+++ src/Demo.WebApi/Program.cs
@@ -1,17 +1,18 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+app.MapOpenApi();
+app.MapScalarApiReference("/scalar", opts =>
+{
+    opts.DarkMode = true;
+});
 
 app.UseHttpsRedirection();
 
